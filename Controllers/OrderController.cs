d48e254b411a2c38632f2799103920c920465e88
Gregory Tanaka <gregoryftanaka@gmail.com>
2020-04-03 21:50:09 -0600

    Add authentication and authorization, course completed

--- Controllers/OrderController.cs
+++ Controllers/OrderController.cs
@@ -1,4 +1,5 @@
 ﻿using csc237_gtanaka_Bethanys.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -7,6 +8,7 @@ using System.Threading.Tasks;
 
 namespace csc237_gtanaka_Bethanys.Controllers
 {
+	[Authorize]
 	public class OrderController : Controller
 	{
 		private readonly IOrderRepository _orderRepository;
--- Startup.cs
+++ Startup.cs
@@ -6,6 +6,7 @@ using csc237_gtanaka_Bethanys.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -29,6 +30,8 @@ namespace csc237_gtanaka_Bethanys
 			services.AddDbContext<AppDbContext>(options => 
 				options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
+			services.AddDefaultIdentity<IdentityUser>().AddEntityFrameworkStores<AppDbContext>();
+
 			services.AddScoped<IPieRepository, PieRepository>();
 			services.AddScoped<ICategoryRepository, CategoryRepository>();
 			services.AddScoped<IOrderRepository, OrderRepository>();
@@ -38,6 +41,7 @@ namespace csc237_gtanaka_Bethanys
 			services.AddHttpContextAccessor();
 			services.AddSession();
 			services.AddControllersWithViews();
+			services.AddRazorPages();
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -53,12 +57,16 @@ namespace csc237_gtanaka_Bethanys
 			app.UseSession();
 			
 			app.UseRouting();
+			app.UseAuthentication();
+			app.UseAuthorization();
+			
 
 			app.UseEndpoints(endpoints =>
 			{
 				endpoints.MapControllerRoute(
 					name: "default",
 					pattern: "{controller=Home}/{action=Index}/{id?}");
+				endpoints.MapRazorPages();
 			});
 		}
 	}
